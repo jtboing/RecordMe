a7de7b6d529867755cd8544da52cf070d7cf471b
Joseph <eric.praline3@gmail.com>
2017-09-28 22:14:27 +0700

    Initial Commit

---
+++ Toastmasters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toastmasters
+//
+//  Created by Joseph Tobing on 9/28/17.
+//  Copyright © 2017 Joseph Tobing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
